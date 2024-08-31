05f638c3bd7bebecb087869d0aae45bed8786d9e
rohanmaiti <maitirohan2019@gmail.com>
2024-08-31 15:32:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const age = 22;
+// stored all the information inside ./git folder that is hidden;
+
+// by typing git add index.js i am telling the vcs to track the index.js file from now .
+// untill then it was untracked  :
+// if am removing something as well it also being tracked
+
+// by typing git diff its telling me what are the changes in the file 
---
+++ script.js
@@ -0,0 +1,11 @@
+function add(a , b){
+    return a + b;
+}
+// this file is also initially untracked and to add this file vcs,
+// we have to again write git add script.js 
+// but every time we will not be doing this 
+// we will type git add .
+// so that every file would get tracked by vcs
+
+// if you dont want to track a file by vcs 
+// then type git rm <filepath>
