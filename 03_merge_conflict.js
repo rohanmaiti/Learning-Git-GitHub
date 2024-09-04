5f157095871674eb008287583dd09a8f13c954b9
rohanmaiti <maitirohan2019@gmail.com>
2024-09-04 16:13:41 +0530

    commit-1

---
+++ 03_merge_conflict.js
@@ -0,0 +1,11 @@
+/**
+ * this is a line 
+ * 
+ */
+
+// commit -1
+// this is commit one and this is i am writing in java script 
+// adding some another line of code 
+let age = 20;
+let name = "Rohan Maiti"
+let collegeName = "Chitkara University"
