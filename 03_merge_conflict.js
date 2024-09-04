55461d10f19e931f80559083d31fb844ad0fc367
rohanmaiti <maitirohan2019@gmail.com>
2024-09-04 16:16:44 +0530

    age changed

--- 03_merge_conflict.js
+++ 03_merge_conflict.js
@@ -6,6 +6,6 @@
 // commit -1
 // this is commit one and this is i am writing in java script 
 // adding some another line of code 
-let age = 20;
+let age = 34;
 let name = "Rohan Maiti"
 let collegeName = "Chitkara University"
